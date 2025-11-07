2fdd80e1a530c70d1e1324651e78b7fabc6dfb96
Andreas Lohne <andreas@Andreas-sin-MacBook-Air.local>
2025-11-07 13:32:02 +0100

    adding js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+ var navn = "Andreas";
+ var alder = 67
+
+ const isLoggedin = true;
+
+
+ console.log(navn);
+ console.log(alder);
+
+
+
+
+
+
+
+window.addEventListener("load", () =>{
+    window.scrollTo({top:0})
+})  
+
+
+
+
+
